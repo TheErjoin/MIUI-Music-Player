6e49be9668bea6929e264d74e32e3e1dfc67c066
Erjan04 <theerjoinlast@gmail.com>
2022-12-31 00:26:32 +0300

    splash screen added

--- app/src/main/java/kg/erjan/musicplayer/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/kg/erjan/musicplayer/presentation/ui/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kg.erjan.musicplayer.presentation.ui.navigation.MainGraphNavigation
 import kg.erjan.musicplayer.presentation.ui.theme.MainBackgroundColor
@@ -16,7 +17,9 @@ import kg.erjan.musicplayer.presentation.ui.theme.MusicPlayerTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         requestPermission()
         setContentCompose()
     }
